8e00e3d043f2589d9f0cc2ad95bddf47e6ca624c
Yasirah Krueng <yasirah.krueng@arundo.com>
2020-01-14 19:04:11 -0600

    initial commit: Welcome to Jangos Island

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  cardContainer: {
+  },
+  card: {
+    margin: '0 auto',
+    maxWidth: 600
+  },
+  media: {
+    height: 140
+  }
+});
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.cardContainer}>
+      <Card className={classes.card}>
+        <CardContent>
+          <Typography align='center' gutterBottom variant="h5" component="h2">
+            Welcome to Jangos Island
+          </Typography>
+        </CardContent>
+      </Card>
     </div>
   );
 }
