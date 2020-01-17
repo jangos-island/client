f444f79866cee007eed34aaee66295898039f404
Yasirah Krueng <yasirah.krueng@arundo.com>
2020-01-16 20:16:09 -0600

    add responsive grid layout to home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,21 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 
 const useStyle = makeStyles(theme => ({
-  container: {
-    width: "100%",
+  root: {
+    flexGrow: 1,
+  },
+  header: {
     textAlign: "center"
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary
   }
 }));
 
@@ -15,14 +24,38 @@ function Home({ setToken }) {
 
   const handleLogout = () => setToken(null);
   return (
-    <div className={classes.container}>
-      <Typography align="center" variant="h3">
-        Welcome to the Jango's Island
-      </Typography>
-      <Button onClick={handleLogout} color="primary" variant="outlined">
-        Logout
-      </Button>
-    </div>
+      <div className={classes.root}>
+        <Grid container spacing={3}>
+          <Grid className={classes.header} item xs={12} md={12}>
+            <Typography align="center" variant="h3">
+              Welcome to the Jango's Island
+            </Typography>
+            <Button onClick={handleLogout} color="primary" variant="outlined">
+              Logout
+            </Button>
+          </Grid>
+          <Grid item xs={12} md={8}>
+            <Paper variant="outlined" className={classes.paper}>
+              Game Viewer
+            </Paper>
+          </Grid>
+          <Grid  item xs={12} md={4}>
+            <Paper variant="outlined" className={classes.paper}>
+              Room Info
+            </Paper>
+          </Grid>
+          <Grid item xs={12} md={12}>
+            <Paper variant="outlined" className={classes.paper}>
+              Extra
+            </Paper>
+          </Grid>
+          <Grid item xs={12} md={12}>
+            <Paper variant="outlined" className={classes.paper}>
+              Footer
+            </Paper>
+          </Grid>
+        </Grid>
+      </div>
   );
 }
 
